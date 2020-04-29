b0c1e335c26dc04d116c782c668429da4d2ed145
Lucas Bodson <alu0101111254@ull.edu.es>
2020-04-29 22:15:23 +0100

    Prueba renderizar escena

---
+++ src/render-scene.js
@@ -0,0 +1,4 @@
+let scene = new THREE.Scene();
+let camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
+let renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, windows.innerHeight);
