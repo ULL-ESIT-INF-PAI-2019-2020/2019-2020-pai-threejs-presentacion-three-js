1627338f37f6b46b1bb92b9c016cdf51bb9c3511
Lucas Bodson <alu0101111254@ull.edu.es>
2020-04-29 22:21:05 +0100

    Prueba crear cubo rojo

--- src/render-scene.js
+++ src/render-scene.js
@@ -1,4 +1,16 @@
+const RED =  "#FF0000";
 let scene = new THREE.Scene();
 let camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
 let renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, windows.innerHeight);
+document.body.appendChild(renderer.domElement);
+let geometryFigure = new THREE.BoxGeometry(1, 1, 1);
+let material = new THREE.MeshBasicMaterial({color: RED});
+let cube = new THREE.Mesh(geometryFigure, material);
+scene.add(cube);
+camera.position.z = 5;
+function render() {
+  requestAnimationFrame(render);
+  renderer.render(scene, camera);
+}
+render();
