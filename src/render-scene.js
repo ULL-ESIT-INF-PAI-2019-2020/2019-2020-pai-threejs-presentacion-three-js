fa650e59d578b879f449668695e0df814d0dee8e
Lucas Bodson <alu0101111254@ull.edu.es>
2020-04-30 00:03:24 +0100

    prueba texturas

--- src/render-scene.js
+++ src/render-scene.js
@@ -1,16 +1,81 @@
-const RED =  "#FF0000";
-let scene = new THREE.Scene();
-let camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
-let renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, windows.innerHeight);
-document.body.appendChild(renderer.domElement);
-let geometryFigure = new THREE.BoxGeometry(1, 1, 1);
-let material = new THREE.MeshBasicMaterial({color: RED});
-let cube = new THREE.Mesh(geometryFigure, material);
-scene.add(cube);
-camera.position.z = 5;
-function render() {
-  requestAnimationFrame(render);
-  renderer.render(scene, camera);
+"use strict";
+window.onload = init;
+
+function init() {
+
+  const RED =  "#FF0000";
+  const BLUE = "#1a1aff";
+  let scene = new THREE.Scene();
+  let camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
+  let renderer = new THREE.WebGLRenderer();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  document.body.appendChild(renderer.domElement);
+  let geometryFigure = new THREE.BoxGeometry(4, 4, 4);
+  //geometryFigure.setSize()
+  let texture = new THREE.TextureLoader().load('../img/face.jpg');
+  let material = new THREE.MeshPhongMaterial({map: texture});
+  material.bumpMap   = new THREE.TextureLoader().load('../img/face.jpg');   
+  material.bumpScale = 0.1;
+
+  let cube = new THREE.Mesh(geometryFigure, material);
+  scene.add(cube);
+  camera.position.z = 5;
+  //camera.position.x = 10;
+  //camera.position.y = 3;
+  camera.lookAt(scene);
+  console.log(camera);
+  render();
+  var light = new THREE.DirectionalLight( 0xffffff );
+  light.position.set( 0, 1, 1 ).normalize();
+  scene.add(light);
+  function render() {
+    requestAnimationFrame(render);
+    cube.rotation.y += 0.005;
+    cube.rotation.x += 0.005;
+    //camera.rotation.x += 0.001;
+    //camera.rotation.y += 0.001;
+    //camera.rotation.z += 0.01;
+    renderer.render(scene, camera);
+
+  }
+
+
 }
-render();
+  /* const RED =  "#FF0000";
+  const BLUE = "#1a1aff";
+  let scene = new THREE.Scene();
+  let camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
+  let renderer = new THREE.WebGLRenderer();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  document.body.appendChild(renderer.domElement);
+  let geometryFigure = new THREE.BoxGeometry(4, 4, 4);
+  //geometryFigure.setSize()
+  let texture = new THREE.TextureLoader().load('../img/astolfo.jpg');
+  let material = new THREE.MeshPhongMaterial({map: texture});
+  material.bumpMap   = new THREE.TextureLoader().load('../img/astolfo.jpg');   
+  material.bumpScale = 0.05;
+
+  let cube = new THREE.Mesh(geometryFigure, material);
+  scene.add(cube);
+  camera.position.z = 5;
+  //camera.position.x = 10;
+  //camera.position.y = 3;
+  camera.lookAt(scene);
+  console.log(camera);
+  render();
+
+  function render() {
+    requestAnimationFrame(render);
+    cube.rotation.y += 0.01;
+    cube.rotation.x += 0.01;
+    //camera.rotation.x += 0.001;
+    //camera.rotation.y += 0.001;
+    //camera.rotation.z += 0.01;
+    renderer.render(scene, camera);
+
+  }
+}
+  */  
+
+
+
